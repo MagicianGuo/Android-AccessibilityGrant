71c4fdf17edf317ef5f7fa8f03dc1d6c2bf293be
MagicianGuo <2639880697@qq.com>
2025-04-26 23:03:22 +0800

    显示应用类型

--- app/src/main/java/com/magicianguo/accessibilitygrant/adapter/AccessibilityListAdapter.kt
+++ app/src/main/java/com/magicianguo/accessibilitygrant/adapter/AccessibilityListAdapter.kt
@@ -1,5 +1,6 @@
 package com.magicianguo.accessibilitygrant.adapter
 
+import android.annotation.SuppressLint
 import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
@@ -32,6 +33,7 @@ class AccessibilityListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>()
         return mList.size
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables", "SetTextI18n")
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val itemBean = mList[position]
         val context = holder.itemView.context
@@ -41,29 +43,40 @@ class AccessibilityListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>()
             holder.tvPackage.text = "应用包名：${itemBean.packageName}"
             holder.tvServiceLabel.text = "辅助功能名称：${itemBean.serviceLabel}"
             holder.tvServiceName.text = "辅助功能服务：${itemBean.serviceName}"
+            if (itemBean.appType == 0) {
+                holder.tvAppType.text = "用户"
+                holder.tvAppType.background = context.getDrawable(R.drawable.bg_icon_app_type_user)
+            } else {
+                holder.tvAppType.text = "系统"
+                holder.tvAppType.background = context.getDrawable(R.drawable.bg_icon_app_type_system)
+            }
             holder.swAccessibility.isChecked = itemBean.enabled
-            holder.swAccessibility.setOnCheckedChangeListener { _, isChecked ->
-                val fullServiceName = "${itemBean.packageName}/${itemBean.serviceName}"
-                val services = Settings.Secure.getString(context.contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES)
-                Log.d("AccessibilityListAdapter", "onBindViewHolder: services = $services")
-                val enabledNames = mutableListOf<String>()
-                if (!services.isNullOrEmpty()) {
-                    enabledNames.addAll(services.split(":"))
-                }
-                enabledNames.remove(fullServiceName)
-                if (isChecked) {
-                    enabledNames.add(fullServiceName)
-                }
-                val result = StringBuilder()
-                for (i in 0 until enabledNames.size) {
-                    if (i != 0) {
-                        result.append(":")
+            // 这里不能使用 setOnCheckedChangeListener ，容易在滚动中触发回调
+            holder.swAccessibility.setOnClickListener { v ->
+                v.postDelayed({
+                    val isChecked = holder.swAccessibility.isChecked
+                    val fullServiceName = "${itemBean.packageName}/${itemBean.serviceName}"
+                    val services = Settings.Secure.getString(context.contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES)
+                    Log.d("AccessibilityListAdapter", "onBindViewHolder: services = $services")
+                    val enabledNames = mutableListOf<String>()
+                    if (!services.isNullOrEmpty()) {
+                        enabledNames.addAll(services.split(":"))
                     }
-                    result.append(enabledNames[i])
-                }
-                Log.d("AccessibilityListAdapter", "onBindViewHolder: isChecked = $isChecked")
-                Log.d("AccessibilityListAdapter", "onBindViewHolder: result = $result")
-                Settings.Secure.putString(context.contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, result.toString())
+                    enabledNames.remove(fullServiceName)
+                    if (isChecked) {
+                        enabledNames.add(fullServiceName)
+                    }
+                    val result = StringBuilder()
+                    for (i in 0 until enabledNames.size) {
+                        if (i != 0) {
+                            result.append(":")
+                        }
+                        result.append(enabledNames[i])
+                    }
+                    Log.d("AccessibilityListAdapter", "onBindViewHolder: isChecked = $isChecked")
+                    Log.d("AccessibilityListAdapter", "onBindViewHolder: result = $result")
+                    Settings.Secure.putString(context.contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, result.toString())
+                }, 100)
             }
         }
     }
@@ -74,6 +87,7 @@ class AccessibilityListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>()
         val tvPackage: TextView = itemView.findViewById(R.id.tv_package)
         val tvServiceLabel: TextView = itemView.findViewById(R.id.tv_service_label)
         val tvServiceName: TextView = itemView.findViewById(R.id.tv_service_name)
+        val tvAppType: TextView = itemView.findViewById(R.id.tv_app_type)
         val swAccessibility: SwitchMaterial = itemView.findViewById(R.id.sw_accessibility)
     }
 
